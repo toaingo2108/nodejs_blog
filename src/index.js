b4b42202ff6360fde1a19cf1827d9f0acc7cf6fb
toaingo2108 <nqtoai2181@gmail.com>
2021-10-17 11:25:48 +0700

    Install prettier lint-staged husky

--- src/index.js
+++ src/index.js
@@ -15,24 +15,24 @@ app.use(
         extended: true,
     }),
 );
-app.use(express.json());
+    app.use(express.json());
 
 // HTTP logger
 // app.use(morgan('combined'))
 
 // Template engine
-app.engine(
+    app.engine(
     'hbs',
     handlebars({
         extname: '.hbs',
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+    app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
-route(app);
+    route(app);
 
-app.listen(port, () => {
+    app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
