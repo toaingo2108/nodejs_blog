e28edcbfc20875bacfeb1801aa0108976480208a
toaingo2108 <nqtoai2181@gmail.com>
2021-10-17 19:39:42 +0700

    Create course with POST method

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,19 @@
+const Course = require('../models/Course');
+const {
+    multipleMongooseToObject,
+    mongooseToObject,
+} = require('../../util/mongoose');
+
 class SiteController {
     // [GET] /
-    index(req, res) {
-        res.render('home');
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
 
     // [GET] /search
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 
+const route = require('./routes/index');
+const db = require('./config/db');
+
+// Connect to DB
+db.connect();
+
 const app = express();
 const port = 3000;
-
-const route = require('./routes/index');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,11 +32,11 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
